31769e08fdaf8cf07f08d39388017c7b536bc07f
Chems <87978998+Chems08@users.noreply.github.com>
2021-11-30 12:33:16 +0100

    Update chiffrement_vernam.py

--- chiffrement_vernam.py
+++ chiffrement_vernam.py
@@ -2,24 +2,20 @@
 # L'algorithme doit respecter les conditions suivantes : 
 
 #   - le mot choisit ne doit pas excéder 5 lettres
-#   - le nombre pour la clé d'une lettre ne doit pas excéder 52
+#   - le nombre pour la clé d'une lettre ne doit pas excéder 67
 
 
-# Testé avec le mot 'hello' et la clé de chiffrement : [1 10 2 15 3]
+# Testé avec le mot 'hello' et la clé de chiffrement : [1 10 2 15 3] et l'alphabet dans l'ordre : 
 # --> Résultat attendu : 'ionar'
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# Pb de l'algorithme :
+import random
+# 2 fois l'alphabet avec caractères
 
-#   - impossibilité de choisir la taille du mot et de la clé par conséquent (5 lettres requises)
-#   - impossibilité d'excéder un certain nombre pour la clé d'une lettre (max 52)  
-
-#-----------------------------------------------------------------------------------------------------------------------------------------------------------
-
-# 3 fois l'alphabet
-
-i = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"]
+k = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","@","&","!","-","#","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9","@","&","!","-","#"]
+i = "".join(random.sample(k, len(k)))
+print("L'alphabet de chiffrement est :", i)
 
 # mot en clair
 
@@ -30,18 +26,25 @@ mot = list(mot)
 # mot = ["h","e","l","l","o"]
 
 # la clés doit être égale au nombre de lettres dans le mot (5)
-a = []
-a1 = input("Donner la clé de chiffrement pour la lettre 1 : ")
-a.append(a1)
-a2 = input("Donner la clé de chiffrement pour la lettre 2 : ")
-a.append(a2)
-a3 = input("Donner la clé de chiffrement pour la lettre 3 : ")
-a.append(a3)
-a4 = input("Donner la clé de chiffrement pour la lettre 4 : ")
-a.append(a4)
-a5 = input("Donner la clé de chiffrement pour la lettre 5 : ")
-a.append(a5)
-print('La clé de chiffrement est : ', a)
+
+#a = []
+#a1 = input("Donner la clé de chiffrement pour la lettre 1 : ")
+#a.append(a1)
+#a2 = input("Donner la clé de chiffrement pour la lettre 2 : ")
+#a.append(a2)
+#a3 = input("Donner la clé de chiffrement pour la lettre 3 : ")
+#a.append(a3)
+#a4 = input("Donner la clé de chiffrement pour la lettre 4 : ")
+#a.append(a4)
+#a5 = input("Donner la clé de chiffrement pour la lettre 5 : ")
+#a.append(a5)
+
+t = input("Donner la clé de chiffrement (espacé de '-') :")
+
+a = t.split('-')
+
+print('La clé de chiffrement est : ', t)
+
 
 #a = [1, 10, 2, 15, 3]
 
@@ -49,23 +52,34 @@ print('La clé de chiffrement est : ', a)
 
 #m = {"h":1, "e":10, "l":2, "l":15, "o":3}
 
-w = []
+#w = []
 
 # Changement de la valeur des lettres de l'alphabet pour le mot
 
-r1 = int(i.index(mot[0])) + int(a[0])
-r2= int(i.index(mot[1])) + int(a[1])
-r3= int(i.index(mot[2])) + int(a[2])
-r4 = int(i.index(mot[3])) + int(a[3])
-r5 = int(i.index(mot[4])) + int(a[4])
+#r1 = int(i.index(mot[0])) + int(a[0])
+#r2= int(i.index(mot[1])) + int(a[1])
+#r3= int(i.index(mot[2])) + int(a[2])
+#r4 = int(i.index(mot[3])) + int(a[3])
+#r5 = int(i.index(mot[4])) + int(a[4])
 
-w.append(i[r1])
-w.append(i[r2])
-w.append(i[r3])
-w.append(i[r4])
-w.append(i[r5])
+#w.append(i[r1])
+#w.append(i[r2])
+#w.append(i[r3])
+#w.append(i[r4])
+#w.append(i[r5])
+
+r = []
+for l in mot:
+    t = 0
+    while t < len(mot):
+        r.append(i[int(i.index(mot[t])) + int(a[t])])
+        t = t+1
+    break
+
+print(r)
 
 
-print("Le mot chiffré est :","'", ''.join(w),"'")
+
+print("Le mot chiffré est :","'", ''.join(r),"'")
 
 
